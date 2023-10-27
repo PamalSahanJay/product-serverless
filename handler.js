7c081e533fc9b82c7efef10ea37cd6ae4166e0d8
psahan <psahan@wiley.com>
2023-10-27 08:52:31 +0530

    crud finished

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 'use strict';
 require('dotenv').config();
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, UpdateCommand, DeleteCommand, ScanCommand, GetCommand } = require("@aws-sdk/lib-dynamodb");
 const { returnFunction } = require("./statusCode")
 const documentClient = new DynamoDBClient({ region: "us-east-1" });
 const docClient = DynamoDBDocumentClient.from(documentClient);
@@ -44,6 +44,60 @@ module.exports.updateProduct = async (event) => {
     const command = new UpdateCommand(params);
     await docClient.send(command);
     return returnFunction(200, data)
+  } catch (error) {
+    return returnFunction(500, error.message)
+  }
+};
+
+module.exports.deleteProduct = async (event) => {
+  let productId = event.pathParameters.id;
+  try {
+    const params = {
+      TableName: PRODUCT_TABLE_NAME,
+      Key: { productId: productId },
+      ConditionExpression: "attribute_exists(productId)",
+    };
+    const command = new DeleteCommand(params);
+    await docClient.send(command);
+    return returnFunction(200, "product id : " + productId + " is deleted")
+  } catch (error) {
+    return returnFunction(500, error.message)
+  }
+};
+
+module.exports.getAllProducts = async (event) => {
+
+  try {
+    const params = {
+      TableName: PRODUCT_TABLE_NAME
+    };
+    const command = new ScanCommand(params);
+    const result = await docClient.send(command);
+    return returnFunction(200, result.Items)
+  } catch (error) {
+    return returnFunction(500, error.message)
+  }
+};
+
+module.exports.getProduct = async (event) => {
+  let productId = event.pathParameters.id;
+  try {
+    const params = {
+      TableName: PRODUCT_TABLE_NAME,
+      Key: {
+        productId: productId
+      }
+    };
+    const command = new GetCommand(params);
+    const result = await docClient.send(command);
+
+    if (result.Item) {
+      return returnFunction(200, result.Item)
+    }
+    else {
+      return returnFunction(404, "Item : " + productId + " is not found")
+    }
+
   } catch (error) {
     return returnFunction(500, error.message)
   }
